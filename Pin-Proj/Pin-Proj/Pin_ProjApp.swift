a69b3d6c6ad00187f516396149571fa0093e825f
Mary Adebayo <mary.modupe04@gmail.com>
2025-06-01 17:14:41 -0500

    Add files via upload

---
+++ Pin-Proj/Pin-Proj/Pin_ProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pin_ProjApp.swift
+//  Pin-Proj
+//
+//  Created by Mary Adebayo on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Pin_ProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
